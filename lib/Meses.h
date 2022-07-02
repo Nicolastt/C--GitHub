b42e140a53515ec5c1409a42d6b66a244fd08aaf
nicholas <gianfrancopr08@gmail.com>
2022-07-02 13:09:42 -0500

    Archivos iniciales

---
+++ lib/Meses.h
@@ -0,0 +1,6 @@
+#define TAB '\t'
+#define ENTER '\n'
+#define PI 3.1416
+#define G 9.81
+
+enum MES {ENE=1, FEB, MAR, ABR, MAY, JUNE, JUL, AGO, SEP, OCT, NOV, DIC};
---
+++ src/1Prueba.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x;
+
+    printf("Ingra un valor para x: ");
+    scanf("%i",&x);
+    printf("El valor de x es: %i \n",x);
+
+    return 0;
+    system("pause");
+}
